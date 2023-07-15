64c5b4e62471392618b5f961d8c0acfadbe36f3c
ARUP GOPE <gopea999@gmail.com>
2023-07-16 02:31:45 +0530

    index page updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 // import { Inter } from 'next/font/google'
-import { useDispatch } from 'react-redux'
+
 
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const dispatch = useDispatch();
+  
   return (
     <main>
       
