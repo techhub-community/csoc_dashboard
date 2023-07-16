3c2a8df27968e63f7a56b3e783869adb916f567b
ARUP GOPE <gopea999@gmail.com>
2023-07-16 22:32:25 +0530

    Added color in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        orange: "#FF904D",
+        lightorange: "#FFBD58",
+        lightyellow: "#FEEA9D",
+        skincolor:"#FFBD58",
+        easy:"#FAE7C4",
+        redishorange:"#EF7E4C",
+        reddish:"#FB3800",
+        greyy:"#F4EEE9",
+        orange2:"#FE995C",
+
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
