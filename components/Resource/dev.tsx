be10bdad3ada816ba231fd0f376d713b968ce804
ARUP GOPE <gopea999@gmail.com>
2023-07-18 18:25:08 +0530

    Resources page Added

---
+++ components/Resource/dev.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { devSourceItem } from "@/constant/Resources/Dev_resources";
+
+export default function Dev() {
+  const commonStyle = "box-border p-3 bg-cream rounded-lg ml-2";
+  return (
+    <>
+      <div className="text-lightblack p-3 h-max md:h-64 w-max md:w-1/2  rounded-lg bg-whitish my-2 mr-3 md:mr-2">
+        <h2 className="text-4xl font-bold ml-3 md:ml-0 ">
+          Developmet Resource
+        </h2>
+        <div className="text-3xl mt-6 overflow-y-auto w-6/6 h-max sm:Overflow-x-auto">
+          <div className="flex flex-col space-y-2">
+            {devSourceItem?.map((devSourceItem, index) => (
+              <span key={index} className={commonStyle}>
+                <a href={devSourceItem.link} key={index}>
+                  {devSourceItem.name}
+                </a>
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ pages/resources/index.tsx
@@ -0,0 +1,6 @@
+import Resouces from "@/components/Resource"
+
+export default function ResourcesPage(){
+  return <Resouces/>
+  
+}
