d618bc9133a6081b5a7d96ca527e7a37c93f1114
Anushka-Singh1 <anushka.sng2002@gmail.com>
2023-07-18 05:59:03 +0530

    optimization and responsiveness of profile page

--- components/PROFILE/Member.tsx
+++ components/PROFILE/Member.tsx
@@ -1,39 +1,50 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 function Member() {
+  const Style =
+    "appearance-none block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-2 px-6 mb-1 leading-tight focus:outline-none focus:bg-white focus:border-gray-500";
+  const type = "text";
+  const Disable = true;
+  const Team = [
+    {
+      placeholder: "Member 1",
+    },
+    {
+      placeholder: "Member 2",
+    },
+    {
+      placeholder: "Member 3",
+    },
+  ];
   return (
-    
-      
-      <div className="flex justify-end mr-[1.5vw] mt-[5.3vw] rounded-lg ">
-      <div className="max-w-sm rounded overflow-hidden shadow-lg bg-[#fef9f4]">
-      <Image
-          className="h-auto max-w-full mt-4" 
-          src="/community.png"
-          alt="me"
-          width={330}
-          height={40}
-        />
+    <div className="flex justify-end mr-[2vw] mt-2 md:mt-28 ml-1 md:ml-0  ">
+      <div className=" w-full rounded overflow-hidden shadow-lg bg-nav_color rounded-lg">
+        <div className="flex justify-center">
+          <Image
+            className="h-auto   mt-4 ml-3 mr-3"
+            src="/community.png"
+            alt="me"
+            width={250}
+            height={30}
+          />{" "}
+        </div>
 
         <div className="px-3 py-1 ">
           <div className="font-bold text-xl text-black mb-1">Team Members</div>
-          <div>
-            <input className="appearance-none block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-2 px-6 mb-1 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="Member 1" readOnly/>
-          </div>
-          <div>
-            <input className="appearance-none block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-2 px-6 mb-1 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="Member 2" readOnly />
-          </div>
-          <div>
-            <input className="appearance-none block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-2 px-6 mb-1 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="Member 3" readOnly/>
-          </div>
+          {Team?.map((item, index) => (
+            <div key={index}>
+              <input
+                className={Style}
+                type={type}
+                placeholder={item.placeholder}
+                readOnly={Disable}
+              />
+            </div>
+          ))}
         </div>
-       
       </div>
-
-
-
-
     </div>
-  )
+  );
 }
 
-export default Member
+export default Member;
