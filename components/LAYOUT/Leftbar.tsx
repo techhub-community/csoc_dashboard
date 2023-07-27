71398e7869a36264e2ad4957af0594f79e8a9578
ARUP GOPE <gopea999@gmail.com>
2023-07-27 17:53:09 +0530

    Hambuger updated

--- components/LAYOUT/Leftbar.tsx
+++ components/LAYOUT/Leftbar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 import Image from "next/image";
+const commonStyle = "w-36 md:w-full py-2 px-4 bg-orangee hover:bg-orange-500 text-white rounded";
 
 const Leftbar: React.FC = () => {
   return (
-    <div className="fixed top-0 left-0 bottom-0 bg-whitish w-24 md:w-1/5 mdp-4 hidden md:block ">
+    <div className="fixed top-0 left-0 bottom-0 bg-whitish w-24 md:w-1/5 mdp-4">
       <div className="flex items-center justify-center mb-8 o">
         <div className="flex items-center">
           <Image
@@ -17,13 +18,13 @@ const Leftbar: React.FC = () => {
         </div>
       </div>
       <div className="space-y-4">
-        <button className="mt-4 md:mt-0 w-36 md:w-full py-2 px-4 bg-orangee hover:bg-orange-500 text-white rounded">
+        <button className={commonStyle}>
           DSA
         </button>
-        <button className="w-36 md:w-full py-2 px-4 bg-orangee hover:bg-orange-500 text-white rounded">
+        <button className={commonStyle}>
           DEVELOPMENT
         </button>
-        <button className="w-36 md:w-full py-2 px-4 bg-orangee hover:bg-orange-500 text-white rounded">
+        <button className={commonStyle}>
           RESOURCES
         </button>
       </div>
