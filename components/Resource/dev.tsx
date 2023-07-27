dad5b0ca7c1903f367756b704d2688d48edb27cf
ARUP GOPE <gopea999@gmail.com>
2023-07-28 01:22:44 +0530

    resources background fixed

--- components/Resource/dev.tsx
+++ components/Resource/dev.tsx
@@ -5,9 +5,9 @@ export default function Dev() {
   const commonStyle = "box-border p-3 bg-cream rounded-lg ml-2";
   return (
     <>
-      <div className="text-lightblack p-3 h-max md:h-64 w-max md:w-1/2  rounded-lg bg-whitish my-2 mr-3 md:mr-2">
+      <div className="text-lightblack p-3 h-max md:h-full w-max md:w-1/2  rounded-lg bg-whitish my-2 mr-3 md:mr-2">
         <h2 className="text-4xl font-bold ml-3 md:ml-0 ">
-          Developmet Resource
+          Development Resource
         </h2>
         <div className="text-3xl mt-6 overflow-y-auto w-6/6 h-max sm:Overflow-x-auto">
           <div className="flex flex-col space-y-2">
--- components/Resource/dsa.tsx
+++ components/Resource/dsa.tsx
@@ -5,7 +5,7 @@ export default function Dsa() {
   const commonStyle = 'box-border p-3 bg-cream rounded-lg ml-2';
   return (
     <>
-      <div className="text-lightblack p-3 h-max md:h-64 w-max md:w-1/2  rounded-lg bg-whitish my-2 mr-3 md:mr-2">
+      <div className="text-lightblack p-3 h-max md:h-full w-max md:w-1/2  rounded-lg bg-whitish my-2 mr-3 md:mr-2">
         <h2 className="text-4xl font-bold ml-0 md:ml-0">DSA Resources <span className='text-whitish'>with do</span> </h2>
         <div className="text-3xl mt-6 overflow-y-auto w-6/6 h-max sm:Overflow-x-auto">
           <div className="flex flex-col space-y-2">
